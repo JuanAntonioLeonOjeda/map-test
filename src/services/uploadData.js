5e3e6f05694c834cc15a6c26ae9485c02376fdbf
JuanAntonioLeonOjeda <jalojr@hotmail.com>
2024-02-20 09:32:46 +0000

    addCoords function created

--- src/services/uploadData.js
+++ src/services/uploadData.js
@@ -2,11 +2,25 @@ import api from "./api";
 
 export const uploadCsv = async (data) => {
   try {
-    const res = await api.post(`/data`, data, {
+    const payload = {
+      body: data,
+      collectionType: 'startups'
+    }
+    const res = await api.post(`/data/demo`, payload, {
       headers: {
         token: localStorage.getItem("token"),
       }
-    });
+    })
+    return res
+  } catch (error) {
+    console.error(error);
+    throw new Error("Cannot send data");
+  }
+};
+
+export const addCoords = async (data) => {
+  try {
+    const res = await api.patch("/division4/coord", { body: data });
     return res;
   } catch (error) {
     console.error(error);
